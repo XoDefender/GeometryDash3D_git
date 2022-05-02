d6680d3e6460b0702459071d37fefddf3f339c13
XoDefender <kir.zhilnikov37@internet.ru>
2022-05-02 22:43:32 +0300

    Smooth ascent and descent
    Here we have particular ascent and descent steps. So, the camera is able to move up and down smoothly according to the steps

--- GeometryDash3D/Assets/Scripts/CameraFollow.cs
+++ GeometryDash3D/Assets/Scripts/CameraFollow.cs
@@ -7,18 +7,80 @@ public class CameraFollow : MonoBehaviour
     private GameObject player;
     private const string playerName = "PlayerTrigger";
 
+    private float offsetX = -42f;
+    private float offsetY = 10f;
+    private float offsetZ = -50f;
+
+    private float followUpOffstet = 7f;
+
+    private float ascentStep = 5f;
+    private float descentStep = 5f;
+
+    private bool readyToMoveUp = false;
+    private bool readyToMoveDown = false;
+
+    private float currentTargetPositionY;
+    private float previousTargetPositionY;
+    private float currentPositionY;
+
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.Find(playerName);
+
+        currentTargetPositionY = offsetY;
+        previousTargetPositionY = offsetY;
+        currentPositionY = player.transform.position.y;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float offsetX = -42f;
-        float offsetY = 10f;
-        float offsetZ = -50f;
-        transform.position = new Vector3(player.transform.position.x + offsetX, offsetY, player.transform.position.z + offsetZ);
+        if(player.transform.position.y >= currentPositionY + followUpOffstet)
+        {
+            readyToMoveUp = true;
+            currentPositionY += followUpOffstet;
+
+            Debug.Log("Ascent = " + currentTargetPositionY);
+        }
+        else if(player.transform.position.y <= currentPositionY - followUpOffstet)
+        {
+            readyToMoveDown = true;
+            currentPositionY -= followUpOffstet;
+
+            Debug.Log("Descent = " + currentTargetPositionY);
+        }
+        else
+        {
+            transform.position = new Vector3(player.transform.position.x + offsetX, currentTargetPositionY, player.transform.position.z + offsetZ);
+
+            Debug.Log("Out");
+        }
+
+        if(readyToMoveUp)
+        {
+            if (currentTargetPositionY < previousTargetPositionY + ascentStep)
+            {
+                currentTargetPositionY += 0.1f;
+            }
+            else
+            {
+                readyToMoveUp = false;
+                previousTargetPositionY = currentTargetPositionY;
+            }
+        }
+
+        if (readyToMoveDown)
+        {
+            if (currentTargetPositionY > previousTargetPositionY - descentStep)
+            {
+                currentTargetPositionY -= 0.1f;
+            }
+            else
+            {
+                readyToMoveDown = false;
+                previousTargetPositionY = currentTargetPositionY;
+            }
+        }
     }
 }
